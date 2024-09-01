acaf2d73e4f201f08cee4116bcd7ba9e4921e410
Hailey Strobelt <haileystrobelt@gmail.com>
2024-08-31 20:06:55 -0600

    Nested Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { 
+  createBrowserRouter, 
+  Route, 
+  createRoutesFromElements,
+  RouterProvider
+} from "react-router-dom";
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Faq from "./pages/help/Faq"
+import Contact from "./pages/help/Contact"
+
+
+//layouts
+import RootLayout from "./layouts/RootLayout"
+import HelpLayout from "./layouts/HelpLayout"
+
+const rounter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} /> 
+      <Route path="about" element={<About />} />
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Route>
+  )
+)
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+      <RouterProvider router={rounter} />
+
   );
 }
 
